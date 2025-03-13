c92422cff276800a31f578a3a60bee9f228c6988
luc <lucaspato7@yahoo.com>
2025-03-13 20:40:40 -0300

    ex-08.py

--- ex-08.py
+++ ex-08.py
@@ -1,2 +1,16 @@
 print('Qual seu nome?')
 input()
+
+
+#Faça um algoritimo para somar dois números
+print('qual o primeiro numero?')
+numero1 = input()
+print('qual o segundo numero?')
+numero2 = input()
+
+numero1 = 10
+numero2 = 20 
+
+soma= numero1 + numero2
+
+print(soma)
