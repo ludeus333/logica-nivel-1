b8fb1a28e527a4c3b7bc2de183a1237678f8f850
luc <lucaspato7@yahoo.com>
2025-03-13 20:50:36 -0300

    ex-08.py

--- ex-08.py
+++ ex-08.py
@@ -4,13 +4,15 @@ input()
 
 #Faça um algoritimo para somar dois números
 print('qual o primeiro numero?')
-numero1 = input()
+numero1 = int(input())
 print('qual o segundo numero?')
-numero2 = input()
-
-numero1 = 10
-numero2 = 20 
+numero2 = int(input())
 
 soma= numero1 + numero2
 
 print(soma)
+
+int = 1,2,3
+strings = 'Joao'
+float
+boleano = true or false 
